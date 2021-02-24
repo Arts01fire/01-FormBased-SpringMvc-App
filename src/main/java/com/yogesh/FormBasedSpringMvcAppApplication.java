2406efc54d8d4e522ec19a101428f3289876ca68
Yogesh Shandilya <yogeshshandilya01@gmail.com>
2021-02-24 16:16:50 +0530

    App modified

--- src/main/java/com/yogesh/FormBasedSpringMvcAppApplication.java
+++ src/main/java/com/yogesh/FormBasedSpringMvcAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class FormBasedSpringMvcAppApplication {
 
+             System.out.println("Form Based Spring Mvc App ....!);
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(FormBasedSpringMvcAppApplication.class, args);
 System.out.println("Welcome To My First War Mvc App...!");
